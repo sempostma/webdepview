4b01972bbef5c926d70845f39e93c65e16aab2ad
Sem Postma <lestergallagher18@gmail.copm>
2020-02-26 11:55:18 +0100

    [ADD] Added license & package.json information & Added npm children size

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,71 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
-const net = require('http')
+const http = require('http')
+const https = require('https')
 const { promisify } = require('util')
-
+const { URL } = require('url')
 const webdepview = require('../')
 
 const readdir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const exists = promisify(fs.exists)
 
 const options = process.argv.filter(x => x.startsWith('--'))
 const args = process.argv.filter(x => !x.startsWith('--'))
+
+const cacheFile = path.join(__dirname, 'cache')
 
 const maxPort = 65535
 
 const favicon = 'AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAABILAAASCwAAAAAAAAAAAAAAAAAAAAAAAAAAAAcAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAUwAAACEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAA4AAAAAAAAAAAAAAAAAAAACAAAAXwAAAM4AAAC4AAAAVQAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABUAAAByAAAAEAAAAAAAAAAAAAAAQAAAALYAAADFAAAAwAAAAJEAAAAtAAAADAAAAAQAAAAAAAAAAAAAAAAAAAAdAAAAtAAAAHAAAAABAAAAEgAAAJoAAADDAAAAjAAAAEYAAABJAAAALAAAABgAAAAYAAAACAAAABAAAAAAAAAAJwAAALYAAAC7AAAALQAAAEAAAACYAAAAlAAAAFoAAAAtAAAABgAAAAIAAAAEAAAAFgAAABUAAAAaAAAAFgAAAC8AAAC/AAAAzQAAAHAAAABnAAAAgwAAAGYAAACJAAAAVAAAAA8AAAABAAAAAgAAAAAAAAABAAAAAQAAAAoAAABOAAAAugAAAMwAAACPAAAAegAAAHsAAABiAAAAjgAAAKgAAABgAAAAIwAAAFsAAAAyAAAAAQAAABwAAAAXAAAAhgAAAN0AAADWAAAAjwAAAHoAAACGAAAAcwAAAMUAAAC6AAAAYwAAADkAAAA9AAAALQAAAAsAAAA+AAAAJQAAAHkAAADcAAAA4AAAAIQAAABVAAAAogAAAKIAAADhAAAAsgAAAEIAAAAiAAAAPAAAABIAAAAPAAAAHgAAADgAAAB8AAAAuQAAAMsAAABlAAAAHgAAAKcAAACdAAAAjQAAAHEAAABUAAAAMQAAAEsAAABoAAAAPwAAAAkAAABVAAAAkAAAAIUAAACqAAAALAAAAAAAAABhAAAAnAAAAHUAAAAnAAAAMAAAAGAAAAChAAAAlwAAAIsAAABOAAAAhAAAAJAAAACbAAAAdgAAAAMAAAAAAAAAEQAAAIcAAAClAAAAhwAAAIAAAAClAAAAoQAAAMgAAADLAAAAmgAAALsAAAC0AAAAkgAAABYAAAAAAAAAAAAAAAAAAAAcAAAAjgAAALIAAACsAAAAtwAAAL8AAAC/AAAArQAAAJ0AAACyAAAAkgAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAABgAAAAnwAAALIAAAC7AAAAsgAAAKoAAACQAAAATwAAAA0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABMAAAA7AAAAUQAAAEkAAAAuAAAADAAAAAAAAAAAAAAAAAAAAAAAAAAAj/IAAMfzAACB8QAAgHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAgAEAAMADAADgBwAA8B8AAA=='
 
+const throttle = (fn, limit, interval) => {
+  if (!Number.isFinite(limit)) {
+    throw new TypeError('Expected `limit` to be a finite number');
+  }
+
+  if (!Number.isFinite(interval)) {
+    throw new TypeError('Expected `interval` to be a finite number');
+  }
+
+  const queue = new Map();
+
+  let currentTick = 0;
+  let activeCount = 0;
+
+  const throttled = function (...args) {
+    let timeout;
+    return new Promise((resolve, reject) => {
+      const execute = () => {
+        resolve(fn.apply(this, args));
+        queue.delete(timeout);
+      };
+
+      const now = Date.now();
+
+      if ((now - currentTick) > interval) {
+        activeCount = 1;
+        currentTick = now;
+      } else if (activeCount < limit) {
+        activeCount++;
+      } else {
+        currentTick += interval;
+        activeCount = 1;
+      }
+
+      timeout = setTimeout(execute, currentTick - now);
+
+      queue.set(timeout, reject);
+    });
+  };
+
+  return throttled;
+};
+
 const createServer = (port = 45033, listener) => new Promise((resolve, reject) => {
-  const server = net.createServer(listener)
+  const server = http.createServer(listener)
     .once('error', err => {
       if (err.code !== 'EADDRINUSE') return reject(err)
       if (port < maxPort) createServer(port + 1, listener).then(resolve).catch(reject)
@@ -48,6 +96,27 @@ const run = async directory => {
 
   const ignoreDev = options.includes('--ignore-dev')
 
+  const dependencySizeCache = await exists(cacheFile)
+    ? JSON.parse(await readFile(cacheFile))
+    : {}
+
+  const npmSize = throttle(url => new Promise((resolve, reject) => {
+    const req = https.request(url, { method: 'HEAD' }, res => {
+      const bytes = +res.headers['content-length']
+      resolve(bytes)
+    })
+    req.on('error', reject)
+    req.end()
+  }), 10, 1000)
+
+  const getDependencySize = async ({ resolved }) => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Determening size of ${resolved}...`);
+    if (Object.prototype.hasOwnProperty.call(dependencySizeCache, resolved)) return dependencySizeCache[resolved]
+    else return dependencySizeCache[resolved] = await npmSize(resolved)
+  }
+
   try {
     const server = await createServer(undefined, async (req, res) => {
       if (req.url === '/favicon.ico') {
@@ -59,7 +128,14 @@ const run = async directory => {
 
       res.writeHead(200, { 'Content-Type': 'text/html' })
 
-      const output = await webdepview({ dependencies, ignoreDev })
+      const output = await webdepview({ dependencies, ignoreDev, getDependencySize })
+
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+
+      await writeFile(cacheFile, JSON.stringify(dependencySizeCache))
+
+      console.log(output)
 
       const html = `<!DOCTYPE html>
 <html lang="en">
@@ -92,9 +168,12 @@ const run = async directory => {
       <th>
         Shared (shared dependencies with other packages)
       </th>
+      <th>
+        Children size gzipped (unique tree of dependencies for this package)
+      </th>
     </thead>
     <tbody>
-    ${output.map(({ name, count, shared }) => {
+    ${output.map(({ name, count, shared, childSize }) => {
         return `<tr>
         <td>
           ${name}
@@ -104,6 +183,9 @@ const run = async directory => {
         </td>
         <td>
           ${shared}
+        </td>
+        <td>
+          ${childSize} bytes
         </td>
       </tr>`
       }).join('')}
